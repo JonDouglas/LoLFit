994ae596c3565166297c7be1e2cf9b6d0c057900
Jon Douglas <jondouglas18@gmail.com>
2015-03-11 00:30:59 -0600

    Any missed items

---
+++ LoLFit.Core/LoLFitApi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using LoLFit.Core.Constants;
+using LoLFit.Core.Model;
+using LoLFit.Core.Model.Enum;
+using ModernHttpClient;
+using Refit;
+
+namespace LoLFit.Core
+{
+    public class LoLFitApi
+    {
+        /// <summary>
+        /// How many requests your app can make every 10 seconds
+        /// </summary>
+        private static int _requestsPerSecond;
+
+        /// <summary>
+        /// How many requests your app can make every 10 minutes
+        /// </summary>
+        private static int _requestsPerMinute;
+
+        
+
+        private static Region _region;
+
+        public LoLFitApi(string apiToken, Region region)
+        {
+            //_apiToken = apiToken;
+
+            _region = region;
+        }
+
+        public async Task<MatchDetails> GetMatchDetails(long matchId)
+        {
+            var client = new HttpClient(new NativeMessageHandler())
+            {
+                BaseAddress = new Uri(String.Format(Endpoints.BaseUrl, _region))
+            };
+
+            var service = RestService.For<ILeagueOfLegendsApi>(client);
+
+            var results = await service.GetMatchDetails(matchId);
+           
+            return results;
+        }
+    }
+}
---
+++ LoLFit.Core/Model/Enum/Season.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoLFit.Core.Model.Enum
+{
+    public enum Season
+    {
+        None,
+        Season3,
+        Season4,
+        Season2015
+    }
+}
